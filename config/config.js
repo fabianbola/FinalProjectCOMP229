f2fdf79a5e3ae18b83275b466a879da1d42f4f0b
OrlandoVelascoRios <ovelasco@my.centennialcollege.ca>
2024-11-07 20:05:34 -0500

    Ad configuration

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    "ATLASDB":"mongodb+srv://dbadmin:ut2KlCsiuOyczIDg@express.zfxod.mongodb.net/products?retryWrites=true&w=majority",
+    "ATLASDB": "mongodb+srv://dbadmin:YVr3YKLvmAZjDlPF@express.oh34o.mongodb.net/project?retryWrites=true&w=majority",
     "SECRETKEY": "e-zcC)&Tm?xhp2[@{nGgF3"
 }

36f1ae7d4b03f584de1f525812b72c3e2be0480a
Fabian <156524829+fabianbola@users.noreply.github.com>
2024-11-18 16:21:12 -0500

    header and fouter

--- frontend/src/components/layout/footer.js
+++ frontend/src/components/layout/footer.js
@@ -1,1 +1,15 @@
-//Fabian
+function footer(){
+    return (<>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <footer>
+            <hr />
+            <p className="text-muted text-center">&copy;&nbsp;Copyright Christian Bocanegra, Orlando Velasco, Brandy Larissa, and Fabian Bolanos - Final group project - COMP229 Web Application Development, third semester</p>
+        </footer>
+    </>
+    );
+}
+
+export default footer;
--- frontend/src/components/layout/header.js
+++ frontend/src/components/layout/header.js
@@ -1,1 +1,52 @@
-//Fabian
+import { Outlet, NavLink, Link } from "react-router-dom";
+import logo from '../../assets/logo.jpg';
+import './header.css';
+
+function Header(){
+    return(
+        <>
+            <center>
+              <Link to="/">
+                <img className='logo' src={logo} alt="Logo image" />
+              </Link>
+            </center>
+            <div className="collapse navbar-collapse" id="collapsibleNavbar">
+            
+                <nav className="navbar">
+                    <ul className="nav-list">
+                        <li>
+                        <NavLink to="/">
+                            <i> </i> All items
+                        </NavLink>
+                        </li>
+                        <li>
+                        <NavLink to="/about">
+                            <i></i> Technology
+                        </NavLink>
+                        </li>
+                        <li>
+                        <NavLink to="/services">
+                            <i></i> Service
+                        </NavLink>
+                        </li>
+                        <li>
+                        <NavLink to="/projects">
+                            <i></i> Projects
+                        </NavLink>
+                        </li>
+                        <li>
+                        <NavLink to="/contactme">
+                            <i></i> Contact me
+                        </NavLink>
+                        </li>
+                    </ul>  
+                </nav>
+
+             </div>
+          <Outlet />
+        </>
+
+    );
+}
+
+export default Header;
